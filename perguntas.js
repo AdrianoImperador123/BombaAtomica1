2faace9e6f9f75ad3c2b35a2a7657e85f157c197
ProgramadorIniciante <00001090526398sp@al.educacao.sp.gov.br>
2024-10-29 09:21:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 2+2=?',
+    'A resposta é 4!'
+)
+
+criaCartao(
+    'Minecraft',
+    'Como é o nome do mob preto? Ele só atacara o jogador, caso olhe diretamente nos olhos dele',
+    'Enderman, se você disse outra coisa, procure ajuda!'
+)
+
+criaCartao(
+    'Anime',
+    'Qual é o protagosnista do anime "Naruto"?',
+    'Naruto.'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do avô de Hitler?',
+    'Johann Georg, Auf der Heide blüht ein kleines Blümelein und das heißt Erika. Heiß von hunderttausend kleinen Bienelein wird umschwärmt Erika. Denn ihr Herz ist voller Süßigkeit, zarter Duft entströmt dem Blütenkleid. Auf der Heide blüht ein kleines blümelein und das heißt Erika.In der Heimat wohnt ein kleines Mägdelein und das heißt Erika. Dieses Mädel ist mein treues Schätzelein und mein Glück, Erika. Wenn das Heidekraut rot-lila blüht, singe ich zum Gruß ihr dieses Lied. Auf der Heide blüht ein kleines Blümelein und das heißt Erika.In mein m Kämmerlein blüht auch ein Blümelein und das heißt Erika. Schon beim Morgengrau n sowie beim Dämmerschein schaut s mich an, Erika. Und dann ist es mir, als spräch es laut Denkst du auch an deine kleine Braut? In der Heimat weint um dich ein Mägdelein und das heißt Erika'
+)
